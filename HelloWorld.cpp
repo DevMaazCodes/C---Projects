dc0651af49d4cca6706cf37ed6c762825ad4b3bf
DevMaazCodes <maaz79800@gmail.com>
2026-05-06 14:37:58 +0530

    Added comment

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 int main() {
+
+    //this is a comment.
+/* This
+is
+a
+   multi-line
+   comment */
+
+
     std::cout << "hello world" << '\n';
     std::cout << "20 LPA loading...." << '\n';
     return 0;
