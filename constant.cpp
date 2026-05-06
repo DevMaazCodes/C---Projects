69dad72a9fcd962206a3766a0d45c52ee47046f3
DevMaazCodes <maaz79800@gmail.com>
2026-05-06 19:49:17 +0530

    Constant learned

---
+++ constant.cpp
@@ -0,0 +1,20 @@
+//any variable declared as const is a constant and cannot be changed after initialization
+#include <iostream>
+
+
+
+const double pi= 3.14159;
+
+double r=56;
+
+const double areaofcircle= pi*r*r;
+int main(){
+std::cout<<areaofcircle;
+}
+
+
+
+
+
+
+
