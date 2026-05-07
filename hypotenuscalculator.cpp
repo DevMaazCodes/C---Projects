7952a20d0db84ea83a49fe25a7efd13ac8c780a2
DevMaazCodes <maaz79800@gmail.com>
2026-05-07 21:57:10 +0530

    Nothing to commit.

--- hypotenuscalculator.cpp
+++ hypotenuscalculator.cpp
@@ -10,7 +10,7 @@ int main(){
     std::cout<<"what is the length of base(b) in your triangle?"<<'\n';
     std::cin>>b;
     c= sqrt(pow(a,2)+pow(b,2));
-    std::cout<<"the length of hypotenus is " <<c;
+    std::cout<<"the length of hypotenus is " <<c << "cm"<<'\n';
 
     return 0;
 }
