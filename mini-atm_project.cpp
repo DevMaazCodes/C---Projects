5c0c12dfbc06ffb207cbdf1e763ae4b619748e70
DevMaazCodes <maaz79800@gmail.com>
2026-05-09 12:06:45 +0530

    added features in atm

--- mini-atm_project.cpp
+++ mini-atm_project.cpp
@@ -3,55 +3,64 @@
 #include <iomanip>
 int main() {
     double balance=1000.0012;
-    int choice;
+    int choice=0;
     double deposit;
     double withdraw;
+    int pass;
     std::cout<<std::fixed<<std::setprecision(2);
+    while(pass!=1234){ 
+    std::cout<<"Hi user, please enter your pin"<<'\n';
+    std::cin>>pass;
+    if (pass!=1234){
+        std::cout<<"Your pin is incorrect"<<std::endl;
+    }
+}
     std::cout <<"***********************************************ATM Menu***********************************************"<<'\n';
-    std::cout <<"1. Check Balance"<<'\n'<<"2. Deposit Money"<<'\n'<<"3. Withdraw Money"<<'\n'<<"4. Exit"<<'\n';
+    while (choice!=4){ std::cout <<"1. Check Balance"<<'\n'<<"2. Deposit Money"<<'\n'<<"3. Withdraw Money"<<'\n'<<"4. Exit"<<'\n';
     std::cout<<"Enter your choice:";
     std::cin>>choice;
     switch (choice){
         case 1:
-        std::cout<<"Your current balance: $"<<balance;
+        std::cout<<"Your current balance: $"<<balance<<'\n';
         break;
 
         case 2:
-        std::cout<<"Enter deposit amount:";
+        std::cout<<"Enter deposit amount:"<<'\n';
         std::cin>>deposit;
         if (deposit<0){
-            std::cout<<"Please enter a valid input!";
+            std::cout<<"Please enter a valid input!"<<'\n';
         }
         else {
             balance+=deposit;
-            std::cout<<"Deposit successful. Your new balance: $"<<balance ;
+            std::cout<<"Deposit successful. Your new balance: $"<<balance<<'\n' ;
         }
         break;
         
         case 3:
-        std::cout<<"Enter withdrawal amount:";
+        std::cout<<"Enter withdrawal amount:"<<'\n';
         std::cin>>withdraw;
         if (withdraw>balance){
-            std::cout<<"Insufficient Balance!";
+            std::cout<<"Insufficient Balance!"<<'\n';
         }
         else if(withdraw<0) {
-            std::cout<<"Please enter a valid amount!";
+            std::cout<<"Please enter a valid amount!"<<'\n';
         }
         else{
             balance-=withdraw;
-            std::cout<<"Withdrawal successful. Your new balance is: $"<<balance;
+            std::cout<<"Withdrawal successful. Your new balance is: $"<<balance<<'\n';
         }
         break;
 
         case 4:
-        std::cout<<"Thank you for using the ATM";
+        std::cout<<"Thank you for using the ATM"<<'\n';
         break;
 
         default: {
-           std::cout<<"Please enter between number 1-4";
+           std::cout<<"Please enter between number 1-4"<<'\n';
         }
 
-    }
+    }}
+   
 
 
     return 0;
