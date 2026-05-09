b0a84384ccd72a29fc2e558da773ae0dc32071b3
DevMaazCodes <maaz79800@gmail.com>
2026-05-09 13:15:06 +0530

    learned for loops

---
+++ forloop.cpp
@@ -0,0 +1,13 @@
+//for loop can be used to repeat the code x amount of times.
+#include <iostream>
+int main(){
+    for (int i=5; i>=1; i--){
+     std::cout<<i<<std::endl;
+
+    }
+
+
+  std::cout<<"start";
+
+    return 0;
+}
