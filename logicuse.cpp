fc9a778d9f533990363365cab4957a5bf5bcb07f
DevMaazCodes <maaz79800@gmail.com>
2026-05-09 14:22:34 +0530

    used loops to create 2d shapes

---
+++ logicuse.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int main(){
+     for (int i=5; i>=0; i--){
+      
+       for (int row=1; row<=i; row++){
+      std::cout<<'*';
+    }
+      
+    std::cout<<std::endl;
+    }
+
+     
+    
+     
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
