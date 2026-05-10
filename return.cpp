2caae4d873bfe31045b56e89ace4e7744666ef0a
DevMaazCodes <maaz79800@gmail.com>
2026-05-10 13:07:29 +0530

    learned the return keyword and made a salary calculator

---
+++ return.cpp
@@ -0,0 +1,39 @@
+//the return keyword returns the value back to the spot where we called the encompassing function.
+//for this we need to change the return type from void to the data type of which the value is returned. ex- int age(){return num1+num2;}
+#include <iostream>
+#include <ctime>
+ #include <string>
+double combatscore(){
+srand (time(NULL));
+double score= (rand()%400)+100;
+return score;  
+}
+std::string rank(double score){
+    
+    if (score<=500 && score>=400){
+        return  "Radiant/Immortal/Ascendant";
+    }
+    else if (score<400 && score>=300){
+        return "Diamond/Platinum/Gold";
+    }
+    else {
+        return "Silver/Bronze/Iron";
+    }
+}
+int main(){
+
+  double score=  combatscore();
+   std::string level= rank(score);
+   std::cout<<"Combat Score= "<<score<<std::endl;
+   std::cout<<"Rank: "<<level<<std::endl;
+   if (level == "Radiant/Immortal/Ascendant" ){
+    std::cout<<"300 LPA";
+   }
+   else if (level == "Diamond/Platinum/Gold"){
+    std::cout<<"30 LPA";
+   }
+   else {
+    std::cout<<"3 LPA";
+   }
+    return 0;
+}
