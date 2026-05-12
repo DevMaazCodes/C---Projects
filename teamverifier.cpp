3963a61ca2cefdf48ab2424bf0f201b2b4480c3f
DevMaazCodes <maaz79800@gmail.com>
2026-05-12 13:11:06 +0530

    implemented passing an array to function in team verifier

--- teamverifier.cpp
+++ teamverifier.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+void findagent(std::string team[], int size, std::string search);
 int main(){
     
     std::string team[]={"Jett", "Reyna", "Clove", "Cypher", "Fade"};
-    std::string search;
+    
+    int size= sizeof(team)/sizeof(team[0]);
     std::cout<<"***************************************************Team Verifier***************************************************"<<'\n';
+    std::string search;
     std::cout<<"Enter your agent to confirm if it's in the team: ";
     std::getline(std::cin, search);
-    
-    bool found= false;
-    for (int i=0; i<sizeof(team)/sizeof(team[0]); i++){
+    findagent(team, size, search);
+   
+
+
+    return 0;
+}
+
+void findagent(std::string team[],int size, std::string search){
+
+ bool found= false;
+    for (int i=0; i<size; i++){
         if (search==team[i]){
             std::cout<<"Your agent is at no. "<<i+1;
             found=true;
@@ -22,5 +33,5 @@ int main(){
 
 
 
-    return 0;
+    
 }
